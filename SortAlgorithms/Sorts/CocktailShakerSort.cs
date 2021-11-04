d92388fd759d63bb005161eaa6e9e438c6538762
theFellandes <57584110+theFellandes@users.noreply.github.com>
2021-11-04 19:19:22 +0300

    Gnome Sort and Cocktail Shaker Sort are added.

---
+++ SortAlgorithms/Sorts/CocktailShakerSort.cs
@@ -0,0 +1,38 @@
+﻿namespace SortAlgorithms.Sorts
+{
+    public class CocktailShakerSort
+    {
+        private static void Swap(int[] intArray, int index1, int index2)
+        {
+            int temp = intArray[index1];
+            intArray[index1] = intArray[index2];
+            intArray[index2] = temp;
+        }
+
+        public static void Sort(int[] intArray)
+        {
+            while (true)
+            {
+                bool flag;
+                int[] start = {1, intArray.Length - 1};
+                int[] end = {intArray.Length, 0};
+                int[] increment = {1, -1};
+                for (int it = 0; it < 2; ++it)
+                {
+                    flag = true;
+                    for (int i = start[it]; i != end[it]; i += increment[it])
+                    {
+                        if (intArray[i - 1] > intArray[i])
+                        {
+                            Swap(intArray, i-1, i);
+                            flag = false;
+                        }
+                    }
+
+                    if (flag)
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ SortAlgorithms/Sorts/GnomeSort.cs
@@ -0,0 +1,28 @@
+﻿namespace SortAlgorithms.Sorts
+{
+    public class GnomeSort
+    {
+        public static void StupidSort(int[] intArray)
+        {
+            for (int i = 1; i < intArray.Length;)
+            {
+                if (intArray[i - 1] <= intArray[i])
+                    ++i;
+                else
+                {
+                    Swap(intArray, i, i-1);
+                    --i;
+                    if (i == 0)
+                        i = 1; 
+                }
+            }
+        }
+
+        private static void Swap(int[] intArray, int index1, int index2)
+        {
+            int temp = intArray[index1];
+            intArray[index1] = intArray[index2];
+            intArray[index2] = temp;
+        }
+    }
+}
